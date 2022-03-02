4aa2d0a81f3c525c3eccf6fff889643510f71862
Yokub1 <yokubibnsafar@gmail.com>
2022-03-02 18:55:57 +0500

    config

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './pages/Form.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<!--<style>-->
+<!--</style>-->
